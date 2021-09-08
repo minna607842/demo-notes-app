1e2631dde64a9636ba50009bc866cf5a31e0cb96
teru <teruforcomp@gmail.com>
2021-09-08 12:43:54 +0900

    Reverting faulty code

--- src/get.js
+++ src/get.js
@@ -1,8 +1,7 @@
 import handler from "./util/handler";
 import dynamoDb from "./util/dynamodb";
 
-// Wrong handler function name
-export const main2 = handler(async (event) => {
+export const main = handler(async (event) => {
   const params = {
     TableName: process.env.tableName,
     // 'Key' defines the partition key and sort key of the item to be retrieved
@@ -14,7 +13,7 @@ export const main2 = handler(async (event) => {
     }
   };
 
-  const result = await dynamoDbLib.call("get", params);
+  const result = await dynamoDb.get(params);
   if ( ! result.Item) {
     throw new Error("Item not found.");
   }
