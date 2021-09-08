19996caba6164816bf9cf3a1cb84c039c8c8e98f
teru <teruforcomp@gmail.com>
2021-09-08 11:34:30 +0900

    Adding a timeout

--- src/get.js
+++ src/get.js
@@ -3,18 +3,23 @@ import dynamoDb from "./util/dynamodb";
 
 export const main = handler(async (event) => {
   const params = {
-    TableName: process.env.TABLE_NAME,
+    TableName: process.env.tableName,
     // 'Key' defines the partition key and sort key of the item to be retrieved
+    // - 'userId': Identity Pool identity id of the authenticated user
+    // - 'noteId': path parameter
     Key: {
-      userId: event.requestContext.authorizer.iam.cognitoIdentity.identityId,
-      // noteId: event.pathParameters.id, // The id of the note from the path
-    },
+      userId: event.requestContext.identity.cognitoIdentityId,
+      noteId: event.pathParameters.id
+    }
   };
 
   const result = await dynamoDb.get(params);
-  if (!result.Item) {
+  if ( ! result.Item) {
     throw new Error("Item not found.");
   }
+
+  // Set a timeout
+  await new Promise(resolve => setTimeout(resolve, 10000));
 
   // Return the retrieved item
   return result.Item;
